7fd36a2e62cedf7a170207ee2811e13abb7e31d3
Deepthi Sebastian <deepthisebastian@gmail.com>
2021-02-11 20:39:30 +1100

    First Commit for Demo

---
+++ src/main/java/com/qa/util/TestUtil.java
@@ -0,0 +1,9 @@
+package com.qa.util;
+
+public class TestUtil {
+	
+	public void Test_util() {
+		System.out.println("Test Util");
+	}
+
+}
---
+++ src/test/java/com/qa/test/LoginTest.java
@@ -0,0 +1,13 @@
+package com.qa.test;
+
+import org.junit.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void logintest() {
+		
+		System.out.println("My Log in susccessful");
+	}
+
+}
